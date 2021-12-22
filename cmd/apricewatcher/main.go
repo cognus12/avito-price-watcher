81cd60c67edf57e8ae2888772189cdef84aa3330
Ivan Zotov <corvuss90@gmail.com>
2021-12-22 23:04:35 +0300

    feat(requestPrice): get dom node with price

--- cmd/apricewatcher/main.go
+++ cmd/apricewatcher/main.go
@@ -1,9 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func findPriceNode(content string) string {
+	startIndex := strings.Index(content, "<span class=\"js-item-price\"")
+
+	var node string
+
+	node = content[startIndex:]
+
+	closeIndex := strings.Index(node, "</span>")
+
+	node = node[0 : closeIndex+7]
+
+	return node
+}
+
+func extractPrice(html string) float64 {
+	node := findPriceNode(html)
+
+	fmt.Println(node)
+
+	// parse html
+
+	return 0
+}
 
 func requestPrice(url string) {
-	fmt.Println(url)
+	response, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", response.StatusCode, response.Status)
+	}
+
+	dataInBytes, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pageContent := string(dataInBytes)
+
+	price := extractPrice(pageContent)
+
+	fmt.Println(price)
 }
 
 func main() {
