5d6ad14e356b2a4ea5918570e2dc9c3b846fdda7
Ivan Zotov <corvuss90@gmail.com>
2022-02-10 23:54:29 +0300

    feat: add simple logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,44 @@
+package logger
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type Logger interface {
+	Info(msg string, v ...interface{})
+	Error(msg string, v ...interface{})
+	Fatal(msg string, v ...interface{})
+	Panic(msg string, v ...interface{})
+}
+
+type logger struct {
+	errorLogger *log.Logger
+	infoLogger  *log.Logger
+}
+
+func New() *logger {
+	errorLogger := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLogger := log.New(os.Stderr, "INFO\t", log.Ldate|log.Ltime)
+
+	return &logger{errorLogger, infoLogger}
+}
+
+func (l *logger) Info(msg string, v ...interface{}) {
+	l.infoLogger.Printf(msg, v...)
+}
+
+func (l *logger) Error(msg string, v ...interface{}) {
+	l.errorLogger.Printf(msg, v...)
+}
+
+func (l *logger) Fatal(msg string, v ...interface{}) {
+	formatted := fmt.Sprintf(msg, v...)
+	l.errorLogger.Fatal(formatted)
+}
+
+func (l *logger) Panic(msg string, v ...interface{}) {
+	formatted := fmt.Sprintf(msg, v...)
+	l.errorLogger.Panic(formatted)
+}
