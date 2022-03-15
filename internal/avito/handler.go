4b6c3936213142762f4dced5bdbcb681d21d69f5
Ivan Zotov <corvuss90@gmail.com>
2022-03-15 22:22:43 +0300

    feat(avito): add subscribe and unsubscribe handlers base

--- internal/avito/handler.go
+++ internal/avito/handler.go
@@ -16,6 +16,11 @@ type handler struct {
 	logger       logger.Logger
 }
 
+type SubscribtionDTO struct {
+	Url   string
+	Email string
+}
+
 func NewHandler(avitoService Service, logger logger.Logger) handlers.Handler {
 	return &handler{
 		AvitoService: avitoService,
@@ -24,7 +29,9 @@ func NewHandler(avitoService Service, logger logger.Logger) handlers.Handler {
 }
 
 func (h *handler) Register(r *httprouter.Router) {
-	r.HandlerFunc(http.MethodGet, "/avito/parse", apperror.Middleware(h.ParseHandler))
+	r.HandlerFunc(http.MethodGet, "/api/info", apperror.Middleware(h.ParseHandler))
+	r.HandlerFunc(http.MethodPost, "/api/subscribe", h.Subscribe)
+	r.HandlerFunc(http.MethodPost, "/api/unsubscribe", h.Unsubscribe)
 }
 
 func (h *handler) ParseHandler(w http.ResponseWriter, r *http.Request) error {
@@ -42,6 +49,46 @@ func (h *handler) ParseHandler(w http.ResponseWriter, r *http.Request) error {
 
 	h.responseJson(w, adInfo)
 	return nil
+}
+
+func (h *handler) Subscribe(w http.ResponseWriter, r *http.Request) {
+	h.logger.Info("Call subscribe method")
+
+	defer r.Body.Close()
+
+	var dto SubscribtionDTO
+
+	err := json.NewDecoder(r.Body).Decode(&dto)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	h.logger.Info("Call subscribe method with %+v", dto)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Call subscribe"))
+}
+
+func (h *handler) Unsubscribe(w http.ResponseWriter, r *http.Request) {
+	h.logger.Info("Call unsubscribe method")
+
+	defer r.Body.Close()
+
+	var dto SubscribtionDTO
+
+	err := json.NewDecoder(r.Body).Decode(&dto)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	h.logger.Info("Call unsubscribe method with %+v", dto)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Call unsubscribe"))
 }
 
 func (h *handler) responseJson(w http.ResponseWriter, s interface{}) error {
